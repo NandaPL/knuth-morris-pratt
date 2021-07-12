3f737657a70de22d65a6db81bc73ec06c0c7de87
vanessadcamara <vanessadcamara@gmail.com>
2021-07-12 19:13:39 -0400

    adicionando código

--- kmp.cpp
+++ kmp.cpp
@@ -1,67 +1,46 @@
-#include <iostream>
-#include <vector>
+// declaramos uma única função de matching que contém a função pi e o algoritmo de kmp
+vector<int> matching(string t, string s)
+{
+    vector<int> p(1001233), match;
+    int n = t.size(), m = s.size();
+    // temos que p[0] sempre é zero porque contém um único char
+    p[0] = 0;
+    for (int i = 1, j = 0; i < m; i++)
+    {
+        /* tentamos encontrar naquela posição j o i correspondente e para isso,
+        fazemos o pre-processamento da palavra buscada e construção do array p, de mesmo
+        tamanho da string s (padrão). Buscamos pelo maior sufixo que também seja prefi-
+        xo próprio, evitando comparações desnecessárias
+        */
+        while (j > 0 && s[j] != s[i])
+        {
+            j = p[j - 1];
+        }
+        if (s[j] == s[i])
+            j++;
+        p[i] = j;
+    }
+    /* Na segunda parte do código, queremos verificar sempre o que
+    é possível reaproveitar para não haver excesso de comparações
+    */
+    for (int i = 0, j = 0; i < n; i++)
+    {
+        /*o j recebe sempre o índice do que vamos comparar a seguir
+        e tudo o que vem para trás dele já foi comparado e dá match
+        entre as strings s e t
+        */
+        while (j > 0 && t[i] != s[j])
+            j = p[j - 1];
 
-using namespace std;
-
-void kmp(string texto, string padrao, vector<int> aux){
-	int idx_texto = 0, idx_padrao = 0;
-
-	while((unsigned)idx_texto < texto.size()){
-		if(padrao[idx_padrao] == texto[idx_texto]){
-			idx_padrao++;
-			idx_texto++;
-		}
-
-		if((unsigned)idx_padrao == padrao.size()){
-			cout << "Padrao encontrado na posicao: " << (idx_texto - idx_padrao) << endl;
-			cout << "Indice texto: " << idx_texto << endl;
-			idx_padrao = aux[idx_padrao - 1];
-		}
-		
-		if((unsigned)idx_texto < texto.size() && padrao[idx_padrao] != texto[idx_texto]){
-			if(idx_padrao){
-				idx_padrao = aux[idx_padrao - 1];
-			} else{
-				idx_texto++;
-			}	
-		}
-	}
-}
-
-/**Pré-processa a palavra padrão buscada e constroi um 
- * array aux de mesmo tamanho da palavra. Busca pelo 
- * maior sufixo que também seja um prefixo, evitando 
- * comparações inúteis (caractere seja examinado 
- * novamente) dando continuidade no casamento de padrões.
- **/
-void prefix(string padrao, vector<int> & aux){
-	aux[0] = 0;
-	int j = 0, i = 1;
-
-	while((unsigned)i < padrao.size()){
-		if(padrao[i] == padrao[j]){
-			j++;
-			aux[i] = j;
-			i++;
-		} else{
-			if(j){
-				j = aux[j - 1];
-			} else {
-				aux[i] = 0;
-				i++;
-			}
-		}
-	}
-}
-
-int main(int argc, char *argv[]){
-	string texto = "ATGGACGACACCGTCAGTGATTGGCGTCCGTCGCGCAGGCTCTCGATGAGCTGATGCTTTGGGCCGAGGACTGCCCCGAAGTCCGGCACCTCGTGCACGCGGATTTCGGCTCCAACAATGTCCTGACGGACAATGGCCGCATAACAGCGGTCATTGACTGGAGCGAGGCGATGTTCGGGGATTCCCAATACGAGGTCGCCAACATCTTCTTCTGGAGGCCGTGGTTGGCTTGTATGGAGCAGGAGACGCGCTACTTCGAGCGGAGGCATCCGGAGCTTGCAGGATCGCCGCGGCTCCGGGCGTATATGCTCCGCATTGGTC";
-	string padrao = "ATTGG";
-
-	vector<int> aux(padrao.size());
-
-	prefix(padrao, aux);
-	kmp(texto, padrao, aux);
-
-	return 0;
+        // caso as posições sejam iguais, o j é incrementado
+        if (s[j] == t[i])
+            j++;
+        /* caso o índice j seja igual ao tamanho do padrão,
+        significa que encontramos um match e marcamos no vetor
+        de matches o índice do match de s e t
+        */
+        if (j == m)
+            match.push_back(i - j + 1);
+    }
+    return match;
 }
